f551e24112f7ee331ff393e735731018481cdb9d
venkatgoud <venkat.goud@gmail.com>
2020-06-16 14:05:45 +0530

    fixed pdf generation using new react-pdf

--- src/components/preview.js
+++ src/components/preview.js
@@ -1,161 +1,151 @@
 import React from "react"
-import { Page, Text, Document, StyleSheet } from '@react-pdf/renderer';
+import { Page, View, Text, Document, StyleSheet } from '@react-pdf/renderer';
 import { PDFViewer } from '@react-pdf/renderer';
 import parser from '../lib/fountain.js'
 
-//inches
-const pageMargins = {
-    left: 1.5,
-    right: 1,
-    top: 1,
-    bottom: 1
-}
-// const headingMargin = 1.5
-const characterMargin = 3.7
-const dialogMargin = 2.5
-const parentheticalMargin = 3.1
 let DPI = 72
 
-// Create styles
 const stylesIndian = StyleSheet.create({
   page: {
-    //for some reason marginRight is not working so using padding instead
     flexDirection: 'col',
-    paddingLeft: pageMargins.left*DPI,
-    paddingRight: pageMargins.right*DPI,
-    paddingTop: pageMargins.top*DPI,
-    paddingBottom: pageMargins.bottom*72,
+    paddingTop: DPI * 1,
+    paddingBottom: DPI * 1,
+    fontSize: 12,
+    fontFamily: "Courier",
+    lineHeight: 1
   },
   scene_heading: {
-    paddingTop: 16,
+    padding: 0,
+    marginLeft: DPI * 1.5,
+    marginRight: DPI * 0.75,
+    fontFamily: "Courier-Bold",
+    textDecoration: "underline"
   },
   action: {
-    textAlign: 'left',
-    paddingTop: 16,
-    paddingRight: '40%'
+    marginLeft: DPI * 1.5,
+    marginRight: DPI * 4
   },
   character: {
-    paddingTop: 16,
-    paddingLeft: '60%',
+    marginLeft: DPI * 6,
+    marginRight: DPI * 1
   },
   parenthetical: {
-    paddingLeft: '60%',
+    marginLeft: DPI * 5.5,
+    marginRight: DPI * 1
   },
   dialogue: {
-    paddingLeft: '60%',
-    fontSize: 14
+    marginLeft: DPI * 5.5,
+    marginRight: DPI * 1
   },
   transition: {
-    textAlign: 'right',
-    paddingTop: 16
+    alignSelf: "flex-end",
+    marginRight: DPI * 0.5
   },
-  ignored: {
-    color: 'red'
+  pageNumbers: {
+    position: 'absolute',
+    top: 40,
+    right: 60,
+    textAlign: 'left'
   }
 });
 
-const styles = StyleSheet.create({
+const standardStyles = StyleSheet.create({
   page: {
-    //for some reason marginRight is not working so using padding
     flexDirection: 'col',
-    paddingLeft: pageMargins.left*DPI,
-    paddingRight: pageMargins.right*DPI,
-    paddingTop: pageMargins.top*DPI,
-    paddingBottom: pageMargins.bottom*72,
-    fontFamily: 'Courier'
-  },
-  scene_heading: {
-    paddingTop: 16,
-    paddingBottom: 16,
-  },
-  action: {
-    textAlign: 'left'
-  },
-  character: {
-    paddingTop: 16,
-    paddingLeft: (characterMargin-pageMargins.left)*DPI
-  },
-  parenthetical: {
-    paddingLeft: (parentheticalMargin-pageMargins.left)*DPI
-  },
-  dialogue: {
-    paddingLeft: (dialogMargin-pageMargins.left)*DPI,
-    fontSize: 14
+    paddingTop: DPI * 1,
+    paddingBottom: DPI * 1,
+    fontSize: 12,
+    fontFamily: "Courier",
+    lineHeight: 1
   },
   transition: {
-    textAlign: 'right',
-    paddingTop: 16
+    alignSelf: "flex-end",
+    marginRight: DPI * 0.5
   },
-  ignored: {
-    color: 'red'
+  parenthetical: {
+    marginLeft: DPI * 3,
+    marginRight: DPI * 1
+  },
+  dialogue: {
+    marginLeft: DPI * 2.5,
+    marginRight: DPI * 2.25
+  },
+  character: {
+    marginLeft: DPI * 3.5,
+    marginRight: DPI * 1
+  },
+  action: {
+    padding: 0,
+    marginLeft: DPI * 1.5,
+    marginRight: DPI * 0.75
+  },
+  scene_heading: {
+    padding: 0,
+    marginLeft: DPI * 1.5,
+    marginRight: DPI * 0.75,
+    fontFamily: "Courier-Bold",
+    textDecoration: "underline"
+  },
+  pageNumbers: {
+    position: 'absolute',
+    top: 40,
+    right: 60,
+    textAlign: 'left'
   }
 });
 
 const MyDocument = (props) => (
   <Document>
-    <Page size="A4" style={props.styles.page}>
-      {generate(props)}
+    <Page size="LETTER" style={props.styles.page}>
+      <Text style={props.styles.pageNumbers} render={({ pageNumber, totalPages }) => (
+        `${pageNumber} / ${totalPages}`
+      )} fixed />
+      <View>
+        {generate(props)}
+      </View>
     </Page>
   </Document>
 )
 
-const Element = ({styles, token, text}) => {
-  return <Text style={styles[token]}>{text}</Text>
-}
+let pdfContent = (tokens, styles) => (
+  tokens.map((token, index) => {
+    switch (token.type) {
+      case 'scene_heading':
+      case 'action':
+      case 'dialogue_end':
+      case 'transition':
+        return <View key={index}>
+          <Text style={styles[token.type]}>{token.text}</Text>
+          <Text style={styles.heading}>{"\n"}</Text>
+        </View>
+      case 'character':
+      case 'dialogue':
+      case 'parenthetical':
+        return <Text key={index} style={styles[token.type]}>{token.text}</Text>
+      default:
+        return <View key={index} />
+    }
+
+  })
+)
 
 function generate(props) {
 
   let tokens = parser.parse(props.content.toString(), true).tokens
 
-  let elements = tokens.map(function (token,i) {
-    let element
-    switch (token.type) {
-      case 'title':
-      case 'credit':
-      case 'author':
-      case 'authors':
-      case 'source':
-      case 'notes':
-      case 'draft_date':
-      case 'date':
-      case 'contact':
-      case 'copyright':
-      case 'page_break':
-      case 'line_break':
-      case 'centered':
-      case 'section':
-      case 'synopsis':
-      case 'note':
-      case 'boneyard_begin':
-      case 'boneyard_end':
-        element = <Element key={i} styles={props.styles} token={'ignored'} text={token.text}/>; break;
-      case 'dialogue_begin':
-      case 'dialogue_end':
-      case 'dual_dialogue_begin':
-      case 'dual_dialogue_end': break; //TODO
-      case 'scene_heading':
-      case 'transition':
-      case 'action':
-      case 'parenthetical':
-      case 'dialogue':
-      case 'character':
-        element = <Element key={i} styles={props.styles} token={token.type} text={token.text}/>; break;
-      default:  if (!token.text) { console.log(token) } break;
-    }
-    return element
-  });
-  return elements;
+  return pdfContent(tokens, props.styles);
+
 }
 
 
 export default class Preview extends React.Component {
-    render() {
-      return <div className="pdf">
-        <PDFViewer style={{width:"100%",height:"90vh"}}>
-          <MyDocument
-            content={this.props.content}
-            styles={this.props.indian ? stylesIndian : styles}/>
-        </PDFViewer>
-      </div>
-    }
+  render() {
+    return <div className="pdf">
+      <PDFViewer style={{ width: "100%", height: "90vh" }}>
+        <MyDocument
+          content={this.props.content}
+          styles={this.props.indian ? stylesIndian : standardStyles} />
+      </PDFViewer>
+    </div>
+  }
 }
